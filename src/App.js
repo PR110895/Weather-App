cb4487bbe4766a6090e1030a020c8dc652fbe7fc
Pratyush <95492798+PR110895@users.noreply.github.com>
2022-03-25 02:29:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// import CityComponent from "./modules/CityComponent";
+
+import { useState } from "react";
+import Axios from "axios";
+import styled from "styled-components";
+import CityComponent from "./modules/CityComponent";
+import WeatherComponenet from "./modules/WeatherinfoComponent";
+// import WeatherComponenet from "./modules/WeatherinfoComponent";
+
+const API_KEY = "c1feacb2804844b8858d68d9df4301a5";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  align-items: center;
+  box-shadow: 0 3px 6px 0 #555;
+  padding: 20px 10px;
+  border-radius: 4px;
+  width: 380px;
+  background: white;
+  font-family: Montserrat;
+`;
+const AppLabel = styled.div`
+  color: black;
+  font-size: 18px;
+  font-weight: bold;
+`;
 
 function App() {
+  const [city, updateCity] = useState();
+  const [weather, updaterWeather] = useState();
+
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const response = await Axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    );
+    updaterWeather(response.data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppLabel>React Weather App</AppLabel>
+      {weather ? (
+        <WeatherComponenet weather={weather} />
+      ) : (
+        <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
+      )}
+    </Container>
   );
 }
 
